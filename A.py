147efd2d9dde385b563b03127dbf31b4bb0db53d
alecha98 <ale_alexandra@mail.ru>
2015-10-06 12:01:01 +0300

    Update A.py

--- A.py
+++ A.py
@@ -16,3 +16,13 @@
 | 8 3 5 4 5 1 |       |
 +-------------+-------+
 '''
+N=int(input())
+k=1
+A=list(map(int, input().split()))
+for i in range (N):
+    a=A[i]
+    for j in range (k,N):
+        b=A[j]
+        if a==b:
+            print(a)
+    k+=1
